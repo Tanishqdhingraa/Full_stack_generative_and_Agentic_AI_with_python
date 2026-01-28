008b8570327a47fc37761ab211df8ab7a49d9e1b
Tanishq dhingra <dhingratanishq58@gmail.com>
2026-01-29 03:33:50 +0530

    loops completed

---
+++ 04_loops/03_tea_orders.py
@@ -0,0 +1,4 @@
+orders = ['chai', 'coffee']
+
+for names in orders:
+    print(f"Your order is ready of {orders}")
---
+++ 04_loops/04_tea_menu.py
@@ -0,0 +1,8 @@
+menu = ["Green", "Lemon", "Spiced", "Mint"]
+
+#enumerate provide a number &&  start means start count start from 10 
+for idx, item in enumerate(menu, start=0):
+    print(f"{idx} : {item} chai")
+
+# start means idx 
+# item is something inside menu 
