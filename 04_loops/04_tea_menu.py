c73fcf5f2fab8decd2891ed23661e195f927bde1
Tanishq dhingra <dhingratanishq58@gmail.com>
2026-01-31 16:08:01 +0530

    loops completed

--- 04_loops/04_tea_menu.py
+++ 04_loops/04_tea_menu.py
@@ -1,4 +1,7 @@
-menu = ["Green", "Lemon", "Spiced", "Mint"]
+# CONCEPT: For Loop + Enumerate
+# DESCRIPTION: Iterates over a list and gets both index and value
 
-for idx, item in enumerate(menu, start=1):
-    print(f"{idx} : {item} chai")
+menu = ["Green", "Lemon", "Spiced", "Mint"]  # list of items
+
+for idx, item in enumerate(menu, start=1):   # loop with index starting from 1
+    print(f"{idx} : {item} chai")            # display index and item
